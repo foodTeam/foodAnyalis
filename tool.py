485db0a4380e2ae5b4f504e5cd4accd3c9591e12
zhuqiuhui <zhuqiuhui@meituan.com>
2017-05-18 23:17:08 +0800

    2017-05-18

--- tool.py
+++ tool.py
@@ -1,14 +1,42 @@
-# tool function
+import pylab as pl
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.font_manager import FontProperties
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+
 def readFile(filePath):
-    file = open(filePath)
-    for line in file:
-        lineStr = line.split(',')
-        meanValue = lineStr[0].split(':')[1]
-        nEstimator = lineStr[2].split(':')[2].split('}')[0]
-        print(meanValue + " " + nEstimator)
-    return meanValue,nEstimator
+
+
+def drawLineGraph(x, y, labelSet, title, xAxisLa, yAxisLa,
+                  xStart, xLime, yStart, yLime):
+    plotSet = []
+    colorSet = ['r', 'b', 'k', 'g', 'm']
+    for lx, ly, co, la in zip(x, y, colorSet, labelSet):
+        plotSet.append(pl.plot(lx, ly, co, label=la))
+    pl.title(title)
+    pl.xlabel(xAxisLa)
+    pl.ylabel(yAxisLa)
+    pl.xlim(xStart, xLime)
+    pl.ylim(yStart, yLime)
+    pl.legend()
+    pl.show()
 
 if __name__ == '__main__':
+    x = []
+    y = []
+    #xAxis = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+    xAxis = range(80, 110, 1)
+
     filePath = "score.txt"
-    [meanValue,nEstimator] = readFile(filePath)
-    
+    meanList,estimatorList = readFile(filePath)
+
+    x.append(xAxis)
+    y.append(meanList)
+
+    title = 'mode accuracy'
+    drawLineGraph(x, y, ['mean score', 'test'],
+                  title, 'the number of tree', 'accuracy',
+                  70, 110, 0.89, 0.905)
---
+++ tune_parameter/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def test():
+    pop = [[0, 1, 0, 1, 0, 1, 0, 1] for i in range(3)]
+    print(pop)
+    print(type(pop))
+    print(pop[0][1])
+
+
+if __name__ == '__main__':
+    # test()
+    print(int(7.98))
